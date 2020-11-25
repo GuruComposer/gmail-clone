ac875d19aeddfc383a9dc7c3741feb084db97d7c
GuruComposer <wesdevoremusic@gmail.com>
2020-11-25 13:29:58 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Gmail-clone
 A front-end for an email client that makes API calls to send and receive emails using vanilla Javascript, JSON and AJAX. Then, the database is updated using Python and Django.
+
+You can create an account, or login and try reading and composing emails using:
+Username: fred@example.com
+Password: password
+Live: https://myawesomegmailclone.herokuapp.com/
--- project3/settings.py
+++ project3/settings.py
@@ -21,13 +21,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '05$4$3aew(8ywondz$g!k4m779pbvn9)euj0zp7-ae*x@4pxr+'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '05$4$3aew(8ywondz$g!k4m779pbvn9)euj0zp7-ae*x@4pxr+'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = True
+# DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['myawesomegmailclone.herokuapp.com']
+
+ALLOWED_HOSTS = ['myawesomegmailclone.herokuapp.com', "127.0.0.1"]
 
 
 # Application definition
